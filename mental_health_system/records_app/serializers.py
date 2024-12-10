1f11d89b6035c9bf99e8607997158a101db765c5
@Mikist <mikeochango3@gmail.com>
2024-12-10 18:54:08 +0300

    create serializers and implement content

---
+++ mental_health_system/records_app/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Customer, Product, Order, OrderItem, Payment
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
