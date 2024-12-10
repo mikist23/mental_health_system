d7b81ca76e12479d5796ab31d441c2a71a4727a8
@Mikist <mikeochango3@gmail.com>
2024-12-10 18:47:38 +0300

    installed the DRF and add it to our project

--- mental_health_system/mental_health_system/settings.py
+++ mental_health_system/mental_health_system/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'records_app'
+    'records_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
