138eb1d151f980475c7faf28edc83ae016eba269
@Mikist <mikeochango3@gmail.com>
2024-12-10 20:16:27 +0300

    update on the settings to include the static files

--- mental_health_system/mental_health_system/settings.py
+++ mental_health_system/mental_health_system/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'records_app/static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- mental_health_system/records_app/views.py
+++ mental_health_system/records_app/views.py
@@ -4,28 +4,28 @@ from .models import Customer, Product, Order, OrderItem
 # Customer Views
 def customer_list(request):
     customers = Customer.objects.all()
-    return render(request, 'customers/customer_list.html', {'customers': customers})
+    return render(request, 'providers/customers/customer_list.html', {'customers': customers})
 
 def customer_detail(request, customer_id):
     customer = get_object_or_404(Customer, id=customer_id)
     orders = Order.objects.filter(customer=customer)
-    return render(request, 'customers/customer_detail.html', {'customer': customer, 'orders': orders})
+    return render(request, 'providers/customers/customer_detail.html', {'customer': customer, 'orders': orders})
 
 # Product Views
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+    return render(request, 'providers/products/product_list.html', {'products': products})
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    return render(request, 'products/product_detail.html', {'product': product})
+    return render(request, 'providers/products/product_detail.html', {'product': product})
 
 # Order Views
 def order_list(request):
     orders = Order.objects.select_related('customer').all()
-    return render(request, 'orders/order_list.html', {'orders': orders})
+    return render(request, 'providers/orders/order_list.html', {'orders': orders})
 
 def order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     order_items = OrderItem.objects.filter(order=order)
-    return render(request, 'orders/order_detail.html', {'order': order, 'order_items': order_items})
+    return render(request, 'providers/orders/order_detail.html', {'order': order, 'order_items': order_items})
